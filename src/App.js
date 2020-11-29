31da29aa61dab3a25e748df8c6868354a56d7adb
jv-jv <jacopo.v@pm.me>
2020-11-29 22:33:48 +0000

    finalised

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 export default function App() {
-  const TIME_REMAINING = 5
-
+  // Variables
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false)
+  const [time, setTime] = useState(15)
   const [valueTextArea, setValueTextArea] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(TIME_REMAINING)
+  const [timeRemaining, setTimeRemaining] = useState(time)
   const [wordsCount, setWordsCount] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
+  const inputRef = useRef(null)
+
+  // useEffect
 
   useEffect(() => {
-    console.log("run")
     if (isPlaying && timeRemaining > 0) {
       setTimeout(() => {
         setTimeRemaining((prevTime) => prevTime - 1)
@@ -23,11 +26,16 @@ export default function App() {
     setWordsCount(countWords(valueTextArea))
   }, [valueTextArea])
 
+  // Functions declarations
+
   function handleClick(e) {
     e.preventDefault()
     setValueTextArea("")
-    setTimeRemaining(TIME_REMAINING)
+    setTimeRemaining(time)
     setIsPlaying(true)
+    setIsSettingsOpen(false)
+    inputRef.current.disabled = false
+    inputRef.current.focus()
   }
 
   function handleChange(e) {
@@ -36,24 +44,50 @@ export default function App() {
   }
 
   function countWords(string) {
-    const h = string
-      // .trim()
-      .split(" ")
-      .filter((e) => e !== "").length
-    return h
+    return string.split(" ").filter((e) => e !== "").length
   }
+
+  // render
 
   return (
     <>
       <h1>Speed Typing Challenge</h1>
       <p>Type as many words as you can</p>
       <textarea
+        ref={inputRef}
         value={valueTextArea}
         onChange={handleChange}
         disabled={!isPlaying}
       />
-      <p>Time Remaining: {timeRemaining}</p>
-      <button onClick={handleClick} disabled={isPlaying}>
+
+      <p className="time-container">
+        Time Remaining: {timeRemaining}
+        <span
+          className="time-info"
+          onClick={() => setIsSettingsOpen((prev) => !prev)}
+        >
+          i
+        </span>
+      </p>
+
+      {isSettingsOpen && (
+        <>
+          <span className="settings-title">
+            Number of seconds available. change below
+          </span>
+          <div className="settings-btns">
+            <button onClick={() => setTime((prevTime) => prevTime - 1)}>
+              -
+            </button>
+            <span>{time}</span>
+            <button onClick={() => setTime((prevTime) => prevTime + 1)}>
+              +
+            </button>
+          </div>
+        </>
+      )}
+
+      <button className="start-btn" onClick={handleClick} disabled={isPlaying}>
         Start
       </button>
       <p>Words count: {wordsCount}</p>
